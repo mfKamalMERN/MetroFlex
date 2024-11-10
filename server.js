149a89d00ca33d78d37fff5929c40786d2dbf69d
mufaisa <mfkamalimdia@gmail.com>
2024-11-10 06:47:59 +0530

    dotenv added.

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 import express from 'express';
+import 'dotenv/config';
 
 const app = express();
 const port = 9000;
 
 
-app.listen(port, () => console.log(`App running at port ${port}`))
+app.listen(port, () => console.log(`App running at port ${port}`));
